131efe844fb2d1a73e4da709f075835d892cb0c2
Iasmin Corrêa Rebelo <iasmin_correa@hotmail.com>
2023-08-31 20:41:10 -0300

    rotas

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
           <Header />
           <main className="appBody">
             <Routes>
-              <Route path="/Dogs" element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="login/*" element={<Login />} />
               <Route
                 path="conta/*"
