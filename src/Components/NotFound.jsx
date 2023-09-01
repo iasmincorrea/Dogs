bb1f1c1d51eaa8120a6de64890db5bf84a881e46
Iasmin Corrêa Rebelo <iasmin_correa@hotmail.com>
2023-08-31 22:10:54 -0300

    rotas github

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
 
 const NotFound = () => {
+  var pathSegmentsToKeep = 1;
+
+  var l = window.location;
+  l.replace(
+    l.protocol +
+      '//' +
+      l.hostname +
+      (l.port ? ':' + l.port : '') +
+      l.pathname
+        .split('/')
+        .slice(0, 1 + pathSegmentsToKeep)
+        .join('/') +
+      '/?/' +
+      l.pathname
+        .slice(1)
+        .split('/')
+        .slice(pathSegmentsToKeep)
+        .join('/')
+        .replace(/&/g, '~and~') +
+      (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+      l.hash,
+  );
+
   return (
     <div className="container mainContainer" style={{ textAlign: 'center' }}>
       <h1
